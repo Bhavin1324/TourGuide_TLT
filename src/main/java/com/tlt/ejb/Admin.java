af1c86d421f355e5e0f75e6daadf6fba1ec2ca17
bhavin <bjariwala26@gmail.com>
2023-02-27 20:24:09 +0530

    ejb crud logic

--- src/main/java/com/tlt/ejb/Admin.java
+++ src/main/java/com/tlt/ejb/Admin.java
@@ -2,21 +2,50 @@
 package com.tlt.ejb;
 
 import com.tlt.entities.PlaceCategory;
+import com.tlt.entities.PlaceMaster;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateful
 public class Admin implements AdminLocal {
-
+    @PersistenceContext(unitName = "TLT_Persistance")
+    EntityManager em;
+    
     @Override
-    public void insertPlaceCategory() {
-        System.out.println("Hello world!!!");
+    public void insertPlace(PlaceMaster data) {
+        if(data == null)
+            return;
+        em.persist(data);
     }
 
     @Override
-    public Collection<PlaceCategory> getAllPlaceCategories() {
-        return new ArrayList<PlaceCategory>();
+    public void deletePlace(String id) {
+        PlaceMaster place = (PlaceMaster) em.find(PlaceMaster.class, id);
+        em.remove(place);
     }
+
+    @Override
+    public void updatePlace(String id, PlaceMaster newPlaceData) {
+       PlaceMaster place = (PlaceMaster) em.find(PlaceMaster.class, id);
+       if(place != null){
+           em.merge(newPlaceData);
+       }
+    }
+
+    @Override
+    public Collection<PlaceMaster> getAllPlaces() {
+       Collection<PlaceMaster> places = em.createNamedQuery("PlaceMaster.findAll").getResultList();
+       return places;
+    }
+
+    @Override
+    public PlaceMaster getPlaceById(String id) {
+        PlaceMaster place = (PlaceMaster) em.find(PlaceMaster.class, id);
+       return place;
+    }
+        
 
 }
--- src/main/java/com/tlt/ejb/AdminLocal.java
+++ src/main/java/com/tlt/ejb/AdminLocal.java
@@ -2,12 +2,16 @@
 package com.tlt.ejb;
 
 import com.tlt.entities.PlaceCategory;
+import com.tlt.entities.PlaceMaster;
 import java.util.Collection;
 import javax.ejb.Local;
 
 
 @Local
 public interface AdminLocal {
-    void insertPlaceCategory();
-    Collection<PlaceCategory> getAllPlaceCategories();
+   void insertPlace(PlaceMaster data);
+   void deletePlace(String id);
+   void updatePlace(String id, PlaceMaster newPlaceData);
+   Collection<PlaceMaster> getAllPlaces();
+   PlaceMaster getPlaceById(String id);
 }
