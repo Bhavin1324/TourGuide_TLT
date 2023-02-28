6fe147180e0301eb06ada231374b432f3e830e55
Kunal <kunaljadhav8460@gmail.com>
2023-02-28 12:59:39 +0530

    EJB Methods or guide created

--- src/main/java/com/tlt/ejb/Admin.java
+++ src/main/java/com/tlt/ejb/Admin.java
@@ -1,6 +1,7 @@
 
 package com.tlt.ejb;
 
+import com.tlt.entities.GuideMaster;
 import com.tlt.entities.PlaceCategory;
 import com.tlt.entities.PlaceMaster;
 import java.util.ArrayList;
@@ -45,6 +46,46 @@ public class Admin implements AdminLocal {
     public PlaceMaster getPlaceById(String id) {
         PlaceMaster place = (PlaceMaster) em.find(PlaceMaster.class, id);
        return place;
+    }
+
+    @Override
+    public Collection<PlaceMaster> getPlacesByCategory(PlaceCategory categoryId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PlaceMaster getPlacesByName(String Name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insertGuide(GuideMaster data) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deleteGuide(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void updateGuide(String id, GuideMaster newGuideData) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public GuideMaster getGuideById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Collection<GuideMaster> getAllGuides() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Collection<GuideMaster> getGuideByPlace(String PlaceId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
         
 
--- src/main/java/com/tlt/ejb/AdminLocal.java
+++ src/main/java/com/tlt/ejb/AdminLocal.java
@@ -1,17 +1,42 @@
-
 package com.tlt.ejb;
 
+import com.tlt.entities.GuideMaster;
 import com.tlt.entities.PlaceCategory;
 import com.tlt.entities.PlaceMaster;
 import java.util.Collection;
 import javax.ejb.Local;
 
-
 @Local
 public interface AdminLocal {
-   void insertPlace(PlaceMaster data);
-   void deletePlace(String id);
-   void updatePlace(String id, PlaceMaster newPlaceData);
-   Collection<PlaceMaster> getAllPlaces();
-   PlaceMaster getPlaceById(String id);
+    //-------------places crud-------
+
+    void insertPlace(PlaceMaster data);
+
+    void deletePlace(String id);
+
+    void updatePlace(String id, PlaceMaster newPlaceData);
+
+    Collection<PlaceMaster> getAllPlaces();
+
+    PlaceMaster getPlaceById(String id);
+
+    Collection<PlaceMaster> getPlacesByCategory(PlaceCategory categoryId);
+
+    PlaceMaster getPlacesByName(String Name);
+    
+    //-------------Guide crud--------
+
+    void insertGuide(GuideMaster data);
+
+    void deleteGuide(String id);
+
+    void updateGuide(String id, GuideMaster newGuideData);
+    
+    GuideMaster getGuideById(String id);
+
+    Collection<GuideMaster> getAllGuides();
+    
+    Collection<GuideMaster> getGuideByPlace(String PlaceId);
+
+    
 }
