609d7afbda571e46fb56237ac1488ef5aaf7ce74
Kunal <kunaljadhav8460@gmail.com>
2023-05-10 15:49:55 +0530

    PathConstant added to gitIgnore

--- src/main/java/com/tlt/constants/PathConstants.java
+++
@@ -1,9 +0,0 @@
-
-package com.tlt.constants;
-
-
-public final class PathConstants {
-    // While copying this project please change this absolute path to you current project upload location
-    public static final String PROFILE_IMG_UPLOAD = "D:\\MscIT_SR\\sem-projects\\Sem8\\Sprint1\\TheLandmarkTour\\src\\main\\webapp\\uploads\\profiles\\";
-    public static final String DEFAULT_USER_IMG = "D:\\MscIT_SR\\sem-projects\\Sem8\\Sprint1\\TheLandmarkTour\\src\\main\\webapp\\uploads\\profiles\\default_user_893423adfKyou.png";
-}
