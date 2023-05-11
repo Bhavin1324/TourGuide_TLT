d8b6e2e03e858a33ab0be31f6a2ba6d514071826
bhavin <bjariwala26@gmail.com>
2023-05-11 15:25:51 +0530

    big fix: registration modal timing

--- src/main/java/com/tlt/cdis/RegisterUserBean.java
+++ src/main/java/com/tlt/cdis/RegisterUserBean.java
@@ -16,6 +16,7 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import org.glassfish.soteria.identitystores.hash.Pbkdf2PasswordHashImpl;
+import org.primefaces.PrimeFaces;
 import org.primefaces.model.file.UploadedFile;
 
 @Named(value = "registerUserBean")
@@ -34,10 +35,13 @@ public class RegisterUserBean implements Serializable {
 
     UploadedFile file;
     String fileName;
+    
+    boolean showDialog;
 
     public RegisterUserBean() {
         userMaster = new UserMaster();
         passHash = new Pbkdf2PasswordHashImpl();
+        showDialog = false;
     }
 
     public UserMaster getUserMaster() {
@@ -72,6 +76,16 @@ public class RegisterUserBean implements Serializable {
         this.contactNumber = contactNumber;
     }
 
+    public boolean isShowDialog() {
+        return showDialog;
+    }
+
+    public void setShowDialog(boolean showDialog) {
+        this.showDialog = showDialog;
+    }
+
+    
+    
     public void register() throws IOException {
 
         boolean uploadStatus = Utils.uploadFile_PF(file, Utils.IMAGE);
@@ -110,5 +124,6 @@ public class RegisterUserBean implements Serializable {
         confirmPass = "";
         contactNumber = "";
         Utils.resetFilesCache();
+        PrimeFaces.current().executeScript("PF('success_dlg').show()");
     }
 }
