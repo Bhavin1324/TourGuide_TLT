3ffc1429f1bf4d235f6a435910b01201862d5aa6
Kunal <kunaljadhav8460@gmail.com>
2023-06-01 12:59:41 +0530

    guiding places complete

--- src/main/java/com/tlt/ejb/GuideLocal.java
+++ src/main/java/com/tlt/ejb/GuideLocal.java
@@ -1,13 +1,12 @@
-
 package com.tlt.ejb;
 
 import com.tlt.entities.AppointmentMaster;
 import com.tlt.entities.GuideMaster;
+import com.tlt.entities.PlaceMaster;
 import com.tlt.utils.GraphUtils;
 import java.util.Collection;
 import java.util.List;
 import javax.ejb.Local;
-
 
 @Local
 public interface GuideLocal {
@@ -16,13 +15,18 @@ public interface GuideLocal {
     Collection<AppointmentMaster> getAllAppointments();
 
     Collection<AppointmentMaster> getAllAppointmentsByGuide(String Username);
-    
-    Collection<AppointmentMaster> getAppointmentsOfGuide(String guide,String status);
-    
+
+    Collection<AppointmentMaster> getAppointmentsOfGuide(String guide, String status);
+
     List<GraphUtils> getMonthlyAppointmentsCount(String gusername);
-    
+
     List<GraphUtils> getMonthlyRevenueOfGuide(String gusername);
-    
+
     void updateAppointmentStatus(AppointmentMaster appointment);
-    
+
+    Collection<PlaceMaster> getAllPlacesOfGuide(String username);
+
+    void removeGuidesPlace(PlaceMaster pm, String username);
+
+    void addGuidesPlace(PlaceMaster pm, String username);
 }
